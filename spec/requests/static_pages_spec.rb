5a286341922dbab4ff4b39a7a285729c40fa3af4
Florentina <gemalescuflorentina@live.com>
2012-05-24 01:00:41 +0300

    Add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,15 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1',:text=>'Career Journal')
     end
-    it "should have the title 'Home'" do
+    
+    it "should have a base title" do
       visit '/static_pages/home'
-      page.should have_selector('title',:text=>"#{base_title} | Home")
+      page.should have_selector('title',:text => "#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',:text=>"| Home")
     end
   end
   
@@ -20,6 +26,7 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_selector('h1',:text=>'Help')
     end
+    
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',:text=>"#{base_title} | Help")
@@ -31,6 +38,7 @@ describe "StaticPages" do
       visit '/static_pages/about'
       page.should have_selector('h1',:text=>'About me')
     end
+    
     it "should have the title 'About'" do
       visit '/static_pages/about'
       page.should have_selector('title',:text=>"#{base_title} | About")
@@ -42,6 +50,7 @@ describe "StaticPages" do
       visit '/static_pages/contact'
       page.should have_selector('h1',:text=>'Contact')
     end
+    
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',:text=>"#{base_title} | Contact")
